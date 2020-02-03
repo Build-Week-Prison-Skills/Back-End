45163ae0ae62bec3417f58f588be7682b465c6bd
jordan <jordan@jordans-MacBook-Air.local>
2020-02-03 20:38:33 +0700

    register endpoint working

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const cors= require('cors');
 const helmet= require('helmet');
 
 const prisonersRouter = require('../data/Routers/Prisoners-Router')
+const authRouter= require ('../auth/authRouter')
 
 const server= express();
 
@@ -10,7 +11,8 @@ const server= express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
-server.use('/api/prisoners', prisonersRouter)
+server.use('/api/prisoners', prisonersRouter);
+server.use('/api/auth', authRouter)
 
 server.get('/', (req,res)=>{
     res.send('test test')
