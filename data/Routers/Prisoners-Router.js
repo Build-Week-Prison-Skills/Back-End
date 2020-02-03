3f14b6b2aea701eaea50430287cc19762d72a322
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-02-03 12:09:04 +0000

    Get all prisoners working

---
+++ data/Routers/Prisoners-Router.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const Prisoners = require('../Models/Prisoners-Model');
+
+
+router 
+    .get('/', async (req, res) => {
+        try {
+            const prisoners = await Prisoners.find('prisoners');
+            res.status(200).json(prisoners);
+        } catch (error) {
+            res.status(500).json({
+                error: 'Issues getting Prisoners from the database'
+            })
+        }
+    })
+
+    module.exports = router;
