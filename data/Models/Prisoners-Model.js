434c1e36e9ff90f1ddc823cd1c70d4e3d061cc8a
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-02-04 10:06:58 +0000

    Prisoners post with auth working

--- data/Models/Prisoners-Model.js
+++ data/Models/Prisoners-Model.js
@@ -2,7 +2,11 @@ const db = require('../db-Config')
 
 module.exports = {
     add,
-    find
+    find,
+    remove,
+    update,
+    findBy,
+    findById
     
 };
 
@@ -16,4 +20,33 @@ async function add(prisoner) {
 
 function find() {
  return db('prisoners');
+}
+
+function findBy(filter) {
+    return db('prisoners').where(filter).first();
+}
+
+function remove (id) {
+    return db('prisoners').where({id}).delete();
+}
+
+function update(id, changes) {
+    return db('prisoners')
+    .where({id})
+    .update(changes)
+    .then(_ => {
+        return find(id)
+    })
+}
+
+function findById(id) {
+    return db('prisoners') 
+        .select(
+            'id',
+            'name',
+            'day_release',
+            'skills'
+        )
+        .where({id})
+        .first();
 }
