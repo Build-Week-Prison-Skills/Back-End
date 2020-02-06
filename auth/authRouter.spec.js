1f3524c36e313696d4951db121d6f3d9ae1ed989
jordan <jordan@jordans-MacBook-Air.local>
2020-02-06 17:07:27 +0700

    added register test

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -2,3 +2,30 @@ const server = require('../api/server')
 const request = require('supertest')
 const db = require('../data/db-Config')
 
+describe('/login', () => {
+
+    it("token login for prisons is working", async () => {
+      const res = await request(server)
+        .post("/api/auth/login")
+        .send({
+    UserName: "bleh",
+    Password: "gtg" 
+        })
+        expect(res.status).toBe(500)
+    });
+  });
+
+
+  describe('/register', () => {
+//expect failure because of case sensitivity
+    it("token login for prisons is working", async () => {
+      const res = await request(server)
+        .post("/api/auth/register")
+        .send({
+    UserName: "bleh",
+    password: "5555",
+    prison_id: 2
+        })
+        expect(res.status).toBe(500)
+    });
+  });
